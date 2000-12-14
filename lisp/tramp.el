7d4b9049e27c4ccb9bf60034e2409a4398570840
grossjoh <grossjoh>
2000-12-14 13:40:13 +0000

    Going back to separating commands with ";" rather than EOL.

--- lisp/tramp.el
+++ lisp/tramp.el
@@ -4,7 +4,7 @@
 
 ;; Author: Kai.Grossjohann@CS.Uni-Dortmund.DE 
 ;; Keywords: comm, processes
-;; Version: $Id: tramp.el,v 1.434 2000/11/17 22:13:36 grossjoh Exp $
+;; Version: $Id: tramp.el,v 1.435 2000/12/14 13:40:13 grossjoh Exp $
 
 ;; This file is part of GNU Emacs.
 
@@ -72,7 +72,7 @@
 
 ;;; Code:
 
-(defconst tramp-version "$Id: tramp.el,v 1.434 2000/11/17 22:13:36 grossjoh Exp $"
+(defconst tramp-version "$Id: tramp.el,v 1.435 2000/12/14 13:40:13 grossjoh Exp $"
   "This version of tramp.")
 (defconst tramp-bug-report-address "emacs-rcp@ls6.cs.uni-dortmund.de"
   "Email address to send bug reports to.")
@@ -3635,7 +3635,7 @@ a subshell, ie surrounded by parentheses."
   (tramp-send-command multi-method method user host
                       (concat (if subshell "( " "")
                               command
-                              (if command tramp-rsh-end-of-line "")
+                              (if command " ; " "")
                               "echo tramp_exit_status $?"
                               (if subshell " )" "")))
   (tramp-wait-for-output)

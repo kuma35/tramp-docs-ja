c4bd646959cf6ffd4f08f055d891e66597b4e5ab
kaig <kaig>
2001-10-16 09:56:58 +0000

    (tramp-open-connection-su): Make sure that user name is given.

--- lisp/tramp.el
+++ lisp/tramp.el
@@ -4,7 +4,7 @@
 
 ;; Author: Kai.Grossjohann@CS.Uni-Dortmund.DE 
 ;; Keywords: comm, processes
-;; Version: $Id: tramp.el,v 2.41 2001/10/10 12:04:05 kaig Exp $
+;; Version: $Id: tramp.el,v 2.42 2001/10/16 09:57:02 kaig Exp $
 
 ;; This file is part of GNU Emacs.
 
@@ -70,7 +70,7 @@
 
 ;;; Code:
 
-(defconst tramp-version "$Id: tramp.el,v 2.41 2001/10/10 12:04:05 kaig Exp $"
+(defconst tramp-version "$Id: tramp.el,v 2.42 2001/10/16 09:57:02 kaig Exp $"
   "This version of tramp.")
 (defconst tramp-bug-report-address "tramp-devel@lists.sourceforge.net"
   "Email address to send bug reports to.")
@@ -3164,7 +3164,9 @@ at all unlikely that this variable is set up wrongly!"
       (error
        "Cannot connect to different host `%s' with `su' connection method"
        host))
-    (tramp-pre-connection multi-method method (or user (user-login-name)) host)
+    (when (not user)
+      (error "Must give user name for `su' connection method"))
+    (tramp-pre-connection multi-method method user host)
     (tramp-message 7 "Opening connection for `%s' using `%s'..." 
 		   (or user (user-login-name)) method)
     (let ((process-environment (copy-sequence process-environment)))
@@ -3175,14 +3177,14 @@ at all unlikely that this variable is set up wrongly!"
                                        tramp-dos-coding-system))
              (p (apply 'start-process
                        (tramp-buffer-name multi-method method 
-                                          (or user (user-login-name)) host)
+                                          user host)
                        (tramp-get-buffer multi-method method 
-                                         (or user (user-login-name)) host)
+                                         user host)
                        (tramp-get-su-program multi-method method)
                        (mapcar
                         '(lambda (x)
                            (format-spec
-                            x (list (cons ?u (or user (user-login-name))))))
+                            x (list (cons ?u user))))
                         (tramp-get-su-args multi-method method))))
              (found nil)
              (pw nil))
@@ -3215,9 +3217,9 @@ at all unlikely that this variable is set up wrongly!"
             (kill-process p)
             (error "`su' failed: %s" (nth 1 found))))
         (tramp-open-connection-setup-interactive-shell
-         p multi-method method (or user (user-login-name)) host)
+         p multi-method method user host)
         (tramp-post-connection multi-method method 
-                               (or user (user-login-name)) host)))))
+                               user host)))))
 
 ;; HHH: Not Changed.  Multi method.  It is not clear to me how this can 
 ;;      handle not giving a user name in the "file name".

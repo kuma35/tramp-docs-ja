426e3f0ccaf2425092185d4e13d625a69521c4e6
Michael Albinus <Michael.Albinus@gmx.de>
2005-10-23 11:48:04 +0000

    * configure.ac: Bump version (to 2.1.5-pre).
    * Makefile.in (clean): Cleanup also subdirectory test.
    (MANIFEST): Exclude tramp2 and .cvsignore.
    
    * lisp/tramp.el (tramp-let-maybe): Add `edebug-form-spec' property.
    (tramp-handle-expand-file-name): Bind `default-directory' locally
    to "/" in order to avoid problems with UNC shares or Cygwin
    mounts.

--- lisp/tramp.el
+++ lisp/tramp.el
@@ -1884,6 +1884,7 @@ The intent is to protect against `obsolete variable' warnings."
      (let ((,variable ,value))
        ,@body)))
 (put 'tramp-let-maybe 'lisp-indent-function 2)
+(put 'tramp-let-maybe 'edebug-form-spec t)
 
 ;;; Config Manipulation Functions:
 
@@ -3359,16 +3360,19 @@ the result will be a local, non-Tramp, filename."
 	    (setq localname (concat uname fname))))
 	;; No tilde characters in file name, do normal
 	;; expand-file-name (this does "/./" and "/../").  We bind
-	;; directory-sep-char here for XEmacs on Windows, which
-	;; would otherwise use backslash.
+	;; `directory-sep-char' here for XEmacs on Windows, which
+	;; would otherwise use backslash.  `default-directory' is
+	;; bound to "/", because on Windows there would be problems
+	;; with UNC shares or Cygwin mounts.
 	(tramp-let-maybe directory-sep-char ?/
-	  (tramp-make-tramp-file-name
-	   (tramp-find-method method user host)
-	   (tramp-find-user   method user host)
-	   (tramp-find-host   method user host)
-	   (tramp-drop-volume-letter
-	    (tramp-run-real-handler 'expand-file-name
-				    (list localname)))))))))
+	  (let ((default-directory "/"))
+	    (tramp-make-tramp-file-name
+	     (tramp-find-method method user host)
+	     (tramp-find-user   method user host)
+	     (tramp-find-host   method user host)
+	     (tramp-drop-volume-letter
+	      (tramp-run-real-handler 'expand-file-name
+				      (list localname))))))))))
 
 (defun tramp-handle-substitute-in-file-name (filename)
   "Like `substitute-in-file-name' for tramp files.

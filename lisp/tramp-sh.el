07eaf40b0df360223ff75e5f6dca0a8544756be7
Michael Albinus <Michael.Albinus@gmx.de>
2012-01-15 15:59:17 +0000

    * tramp-sh.el (tramp-remote-path): Set tramp-autoload cookie.

--- lisp/tramp-sh.el
+++ lisp/tramp-sh.el
@@ -512,9 +512,10 @@ detected as prompt when being sent on echoing hosts, therefore.")
 ;; GNU/Linux (Debian, Suse): /bin:/usr/bin
 ;; FreeBSD: /usr/bin:/bin:/usr/sbin:/sbin: - beware trailing ":"!
 ;; IRIX64: /usr/bin
+;;;###tramp-autoload
 (defcustom tramp-remote-path
-  '(tramp-default-remote-path "/bin" "/usr/bin" "/usr/sbin" "/usr/local/bin"
-    "/local/bin" "/local/freeware/bin" "/local/gnu/bin"
+  '(tramp-default-remote-path "/bin" "/usr/bin" "/usr/sbin"
+    "/usr/local/bin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin"
     "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin"
     "/opt/bin" "/opt/sbin")
   "*List of directories to search for executables on remote host.

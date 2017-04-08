86c7cdaa92b4b9ef4c524d80b3ff324cb01af995
Jeff Clough <kb1vqh@gmail.com>
2017-04-08 09:41:42 +0200

    * tramp-gvfs.el (tramp-gvfs-handle-write-region): * tramp-sh.el (tramp-sh-handle-write-region): Functions now output characters written in addition to file name.  (Bug#354)

--- lisp/tramp-gvfs.el
+++ lisp/tramp-gvfs.el
@@ -1223,7 +1223,10 @@ file-notify events."
 
     ;; The end.
     (when (or (eq visit t) (null visit) (stringp visit))
-      (tramp-message v 0 "Wrote %s" filename))
+      (tramp-message v 0 "Wrote `%s' (%d characters)" filename
+                     (cond ((null start) (buffer-size))
+                           ((stringp start) (length start))
+                           (t (- end start)))))
     (run-hooks 'tramp-handle-write-region-hook)))
 
 

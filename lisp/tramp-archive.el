f660a4369732e27207374463a15e670cb5a3154f
Michael Albinus <michael.albinus@gmx.de>
2018-01-04 12:50:27 +0100

    * tramp-archive.el (tramp-archive-handle-temporary-file-directory): Use `tramp-compat-temporary-file-directory'.

--- lisp/tramp-archive.el
+++ lisp/tramp-archive.el
@@ -536,7 +536,7 @@ offered."
   ;; mounted directory, it is returned as it.  Not what we want.
   (with-parsed-tramp-archive-file-name default-directory nil
     (let ((default-directory (file-name-directory archive)))
-      (temporary-file-directory))))
+      (tramp-compat-temporary-file-directory))))
 
 (defun tramp-archive-handle-not-implemented (operation &rest args)
   "Generic handler for operations not implemented for file archives."

d1095229ceea97a7640fadd13678bc808c2b6cf3
Michael Albinus <michael.albinus@gmx.de>
2018-02-06 08:44:01 +0100

    *** empty log message ***

--- test/tramp-tests.el
+++ test/tramp-tests.el
@@ -1744,8 +1744,8 @@ handled properly.  BODY shall not contain a timeout."
     (substitute-in-file-name "/method:host:/~foo") "/method:host:/~foo"))
   (should
    (string-equal (substitute-in-file-name "/method:host:/path//~foo") "/~foo"))
-  ;; (substitute-in-file-name "/path/~foo") expands only to "/~foo"",
-  ;; if $LOGNAME or $USER is "foo".  Otherwise, it doesn't expand.
+  ;; (substitute-in-file-name "/path/~foo") expands only for a local
+  ;; user "foo" to "/~foo"".  Otherwise, it doesn't expand.
   (should
    (string-equal
     (substitute-in-file-name

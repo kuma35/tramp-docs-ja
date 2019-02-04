f05996ce22463810d9cb3f177eb7fea9753a6763
Michael Albinus <michael.albinus@gmx.de>
2019-02-04 15:58:00 +0100

    * tramp-rclone.el (tramp-default-host-alist): Add empty host.
    * tramp.el (tramp-parse-auth-sources): Require :port.
    (tramp-process-actions): Disable `global-auto-revert-mode'
    temporarily.  (Bug#34196)

--- lisp/tramp.el
+++ lisp/tramp.el
@@ -2907,7 +2907,8 @@ for all methods.  Resulting data are derived from default settings."
   (and tramp-completion-use-auth-sources
        (mapcar
 	(lambda (x) `(,(plist-get x :user) ,(plist-get x :host)))
-	(auth-source-search :port method :max most-positive-fixnum))))
+	(auth-source-search
+	 :port method :require '(:port) :max most-positive-fixnum))))
 
 ;; Generic function.
 (defun tramp-parse-group (regexp match-level skip-regexp)
@@ -4099,17 +4100,23 @@ performed successfully.  Any other value means an error."
   (save-restriction
     (with-tramp-progress-reporter
 	proc 3 "Waiting for prompts from remote shell"
-      (let (exit)
-	(if timeout
-	    (with-timeout (timeout (setq exit 'timeout))
+      ;; `global-auto-revert-mode' could activate remote operations
+      ;; while we aren't ready.  We disable it temporarily.
+      (let ((garm (bound-and-true-p global-auto-revert-mode))
+	    exit)
+	(when garm (global-auto-revert-mode -1))
+	(unwind-protect
+	    (if timeout
+		(with-timeout (timeout (setq exit 'timeout))
+		  (while (not exit)
+		    (setq exit
+			  (catch 'tramp-action
+			    (tramp-process-one-action proc vec actions)))))
 	      (while (not exit)
 		(setq exit
 		      (catch 'tramp-action
 			(tramp-process-one-action proc vec actions)))))
-	  (while (not exit)
-	    (setq exit
-		  (catch 'tramp-action
-		    (tramp-process-one-action proc vec actions)))))
+	  (when garm (global-auto-revert-mode)))
 	(with-current-buffer (tramp-get-connection-buffer vec)
 	  (widen)
 	  (tramp-message vec 6 "\n%s" (buffer-string)))

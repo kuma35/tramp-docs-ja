9c1a00dc01e5335f5f81ea4e18a339cdb5ca7ae2
Michael Albinus <michael.albinus@gmx.de>
2021-02-17 17:30:31 +0100

    * tramp-cmds.el (tramp-version): Adapt docstring.
    * tramp.el (tramp-handle-expand-file-name):
    * tramp-gvfs.el (tramp-gvfs-handle-expand-file-name):
    * tramp-sh.el (tramp-sh-handle-expand-file-name)
    * tramp-smb.el (tramp-smb-handle-expand-file-name):
    * tramp-sudoedit.el (tramp-sudoedit-handle-expand-file-name):
    Handle local "/..".
    
    * tramp-rclone.el (tramp-methods) <rclone>: Adapt `tramp-mount-args'.
    (tramp-rclone-flush-directory-cache): Remove.
    (tramp-rclone-do-copy-or-rename-file)
    (tramp-rclone-handle-delete-directory)
    (tramp-rclone-handle-delete-file)
    (tramp-rclone-handle-make-directory): Don't use that function.
    (tramp-rclone-maybe-open-connection): Fix use of `tramp-mount-args'.
    
    * trampver.el.in (tramp-inside-emacs): New defun.
    * tramp.el (tramp-handle-make-process):
    * tramp-sh.el (tramp-sh-handle-make-process)
    (tramp-sh-handle-process-file, tramp-open-shell): Use it.
    (tramp-get-env-with-u-option): Remove.

--- lisp/tramp.el
+++ lisp/tramp.el
@@ -3197,6 +3197,9 @@ User is always nil."
     (with-parsed-tramp-file-name name nil
       (unless (tramp-run-real-handler #'file-name-absolute-p (list localname))
 	(setq localname (concat "/" localname)))
+      ;; Do not keep "/..".
+      (when (string-match-p "^/\\.\\.?$" localname)
+	(setq localname "/"))
       ;; Do normal `expand-file-name' (this does "/./" and "/../").
       ;; `default-directory' is bound, because on Windows there would
       ;; be problems with UNC shares or Cygwin mounts.
@@ -3845,10 +3848,7 @@ It does not support `:stderr'."
 			     elt (default-toplevel-value 'process-environment))))
 			(setq env (cons elt env)))))
 	       (env (setenv-internal
-		     env "INSIDE_EMACS"
-		     (concat (or (getenv "INSIDE_EMACS") emacs-version)
-			     ",tramp:" tramp-version)
-		     'keep))
+		     env "INSIDE_EMACS" (tramp-inside-emacs) 'keep))
 	       (env (mapcar #'tramp-shell-quote-argument (delq nil env)))
 	       ;; Quote command.
 	       (command (mapconcat #'tramp-shell-quote-argument command " "))
